5bb853e99535b4f81190df11bb4439d8803ff5b6
Dhemy <imdhemy@gmail.com>
2025-02-09 14:58:24 +0100

    chore(test): add setup templates

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
+        globalSetup: ['./tests/__vitest__/global.ts'],
+        setupFiles: ['./tests/__vitest__/unit.ts'],
         coverage: {
             exclude: [
                 ...configDefaults.exclude,
